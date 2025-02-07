41ffad3a5ae8c2e2693c9c5437e64722c5bf88b7
EhwazM <127918270+EhwazM@users.noreply.github.com>
2025-02-07 16:36:12 -0500

    Update

--- Partitions.sh
+++ Partitions.sh
@@ -9,7 +9,7 @@ read -p "Do you need to connect to wi-fi? (y/n)" _YesNotWIFI
 if [ "$_YesNotWIFI" = "y" ]; then
     read -p "Red Name:" _RedName
     read -p "password:" _RedPassword
-    iwctl --passphrase $_RedPassword station wlan0 connect $_RedName
+    iwctl --passphrase "$_RedPassword" station wlan0 connect "$_RedName"
 fi
 
 _PartitionsNames=0
@@ -23,7 +23,7 @@ while [ "$_PartitionsNames" -eq 0 ]; do
     
     if [ "$_YesNot1" = "y" ]; then
         echo "Partition names are set."
-        _PartitionsNames=true
+        _PartitionsNames=1
     elif [ "$_YesNot1" = "n" ]; then
         echo "Restarting the process."
         continue
@@ -32,25 +32,32 @@ while [ "$_PartitionsNames" -eq 0 ]; do
     fi
 done
 
+echo -e "Formatting partitions...\n"
+
 # Formatting partitions
-mkfs.fat -F32 -n "UEFI" /dev/$_EFI
-mkswap -L "SWAP" /dev/$_SWAP
-swapon /dev/$_SWAP
+mkfs.fat -F32 -n "UEFI" /dev/"$_EFI"
+mkswap -L "SWAP" /dev/"$_SWAP"
+swapon /dev/"$_SWAP"
 
 read -p "root name:" _RootName
-mkfs.ext4 -L $_RootName /dev/$_FS
+mkfs.ext4 -L "$_RootName" /dev/"$_FS"
 
-echo "Partitions formatting done."
+echo -e "Partitions have been formatted. \n"
 
 # Mounting
-mount /dev/$_FS /mnt/
-mkdir -p /mnt/boot/efi/
-mount /dev/$_EFI /mnt/boot/efi/
 
-echo "Partitions mounting done."
+echo -e "Mounting partitions... \n"
+
+mount /dev/"$_FS" /mnt/
+mkdir -p /mnt/boot/efi/
+mount /dev/"$_EFI" /mnt/boot/efi/
+
+echo -e "Partitions have been mounted. \n"
 read -p "Do you have Laptop?: (y/n)" _YesNot2
 
-pacman -Sy
+pacman -Sy --noconfirm
+
+echo -e "Installing basic packages for the installation. \n"
 
 if [ "$_YesNot2" = "y" ]; then
     pacstrap /mnt git curl base base-devel neovim linux-zen linux-zen-headers linux-firmware mkinitcpio xf86-input-libinput
@@ -58,7 +65,9 @@ else
     pacstrap /mnt git curl base base-devel neovim linux-zen linux-zen-headers linux-firmware mkinitcpio
 fi
 
-genfstab -p /mnt >> /mnt/etc/fstab
+echo -e "Generating Fstab... \n"
+
+genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
 
 chmod +x Installations/Arch-chroot.sh
@@ -66,7 +75,9 @@ chmod +x Installations/System_requirements.sh
 
 cp Installations/Arch-chroot.sh /mnt/
 
+echo -e "Entering Arch-chroot environment... \n"
+
 arch-chroot /mnt /bin/bash -c /Arch-chroot.sh
 
 umount -R /mnt/
-swapoff /dev/$_SWAP
+swapoff /dev/"$_SWAP"
