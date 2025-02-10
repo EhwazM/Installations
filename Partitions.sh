3794fdcd96aad4413624609622e025ca3f3ca727
EhwazM <127918270+EhwazM@users.noreply.github.com>
2025-02-09 22:07:58 -0500

    Update Partitions.sh

--- Partitions.sh
+++ Partitions.sh
@@ -13,22 +13,17 @@ if [ "$_YesNotWIFI" = "y" ]; then
 fi
 
 _PartitionsNames=0
-while [ "$_PartitionsNames" -eq 0 ]; do
-    echo "Name your partitions after you created them:"
+while true; do
+    echo "\nName your partitions after you created them:"
     read -p "EFI: " _EFI
     read -p "SWAP: " _SWAP
     read -p "FS: " _FS
-    echo "Are you sure this is correct? EFI=$_EFI, SWAP=$_SWAP, FS=$_FS. (y/n)"
-    read _YesNot1
+    echo -e "\nAre you sure this is correct? EFI=$_EFI, SWAP=$_SWAP, FS=$_FS. (y/n)"
+    read _YesNotP
     
-    if [ "$_YesNot1" = "y" ]; then
-        echo "Partition names are set."
-        _PartitionsNames=1
-    elif [ "$_YesNot1" = "n" ]; then
-        echo "Restarting the process."
-        continue
-    else
-        echo "Invalid input, please try again."
+    if [ "$_YesNotP" = "y" ]; then
+        echo -e "\nPartition names are set."
+        break
     fi
 done
 
@@ -39,7 +34,15 @@ mkfs.fat -F32 -n "UEFI" /dev/"$_EFI"
 mkswap -L "SWAP" /dev/"$_SWAP"
 swapon /dev/"$_SWAP"
 
-read -p "root name:" _RootName
+while true; do
+    read -p "root name:" _RootName
+    echo -e "\nIs '$_RootName' the corret name? (y/n):"
+    read _YesNotR
+    if [ "$_YesNotR" = "y" ]; then
+        break 
+    fi
+done
+
 mkfs.ext4 -L "$_RootName" /dev/"$_FS"
 
 echo -e "Partitions have been formatted. \n"
