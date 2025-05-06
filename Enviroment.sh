58abb081d2ed14c1f0b8ad720e9487410de6c21b
EhwazM <127918270+EhwazM@users.noreply.github.com>
2025-05-05 23:25:32 -0500

    Update Enviroment.sh

--- Enviroment.sh
+++ Enviroment.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-echo "What's going to be the mame for the Venv?:"
-read _VenvName
-mkdir -p VirtualEnvs 
-virtualenv -p /usr/bin/python3 ~/VirtualEnvs/$_VenvName
-source ~/VirtualEnvs/$_VenvName/bin/activate
+read -p "What's going to be the mame for the Venv?: " _VenvName
+read -p "What version are you going to use?: " _Version
+
+mkdir -p ~/.virtualenvs
+mkdir -p ~/.pyenv
+
+if [ -n "$_Version" ]; then
+    pyenv install "$_Version"
+    path_to_pyenv = ~/.pyenv/versions/"$_Version"/bin/python3
+else
+    path_to_pyenv = /usr/bin/python3
+fi
+
+virtualenv -p "$path_to_pyenv" ~/.virtualenvs/$_VenvName
+source ~/.virtualenvs/$_VenvName/bin/activate
 pip install --upgrade pip
 pip install numpy matplotlib notebook
 python -m notebook
